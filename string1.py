11f9c980c205429ecd1c230ecbfab419aefd42ef
cfevrius <21692588+cfevrius@users.noreply.github.com>
2022-11-10 15:19:18 -0500

    Reorganized files. Added all tests for warmup2. Added files for string1 solutions and tests.

---
+++ string1.py
@@ -0,0 +1,18 @@
+# Given a string name, e.g. "Bob", return a 
+# greeting of the form "Hello Bob!".
+def hello_name(name):
+    return None
+
+# Given two strings, a and b, return the result 
+# of putting them together in the order abba, e.g. 
+# "Hi" and "Bye" returns "HiByeByeHi".
+def make_abba(a, b):
+    return None
+
+# The web is built with HTML strings like "<i>Yay</i>" 
+# which draws Yay as italic text. In this example, the 
+# "i" tag makes <i> and </i> which surround the word "Yay". 
+# Given tag and word strings, create the HTML string with tags 
+# around the word, e.g. "<i>Yay</i>".
+def make_tags(tag, word):
+    return None
