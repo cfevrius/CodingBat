e307839f45eadb03d2ceffe05c458c8ef53e099d
cfevrius <21692588+cfevrius@users.noreply.github.com>
2022-11-10 15:19:18 -0500

    Reorganized files. Added all tests for warmup2. Added files for string1 solutions and tests.

--- warmup2.py
+++ warmup2.py
@@ -116,3 +116,9 @@ def no_triples(nums):
     # Regex solution:
     # str_nums = ''.join(str(x) for x in nums)
     # return len(re.findall(r'(\d)\1\1', str_nums)) == 0  
+
+# Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value, followed 
+# by the value plus 5, followed by the value minus 1. Additionally the 271 counts even if 
+# the "1" differs by 2 or less from the correct value.
+def has_271(nums):
+    return None
