67370826824bfa6ed750bea1d2e2e48dfef5a010
cfevrius <21692588+cfevrius@users.noreply.github.com>
2022-11-11 13:02:11 -0500

    Added first 10 solutions for String-1 problems.

--- solutions/string1.py
+++ solutions/string1.py
@@ -1,13 +1,13 @@
 # Given a string name, e.g. "Bob", return a 
 # greeting of the form "Hello Bob!".
 def hello_name(name):
-    return None
+    return f"Hello {name}!"
 
 # Given two strings, a and b, return the result 
 # of putting them together in the order abba, e.g. 
 # "Hi" and "Bye" returns "HiByeByeHi".
 def make_abba(a, b):
-    return None
+    return f"{a}{b}{b}{a}"
 
 # The web is built with HTML strings like "<i>Yay</i>" 
 # which draws Yay as italic text. In this example, the 
@@ -15,4 +15,44 @@ def make_abba(a, b):
 # Given tag and word strings, create the HTML string with tags 
 # around the word, e.g. "<i>Yay</i>".
 def make_tags(tag, word):
-    return None
+    return f"<{tag}>{word}</{tag}>"
+
+# Given an "out" string length 4, such as "<<>>", and a word, return 
+# a new string where the word is in the middle of the out string, e.g. "<<word>>". 
+# Note: use str[i:j] to extract the string starting at index i and going 
+# up to but not including index j.
+def make_out_word(out, word):
+    return f"{out[:2]}{word}{out[-2:]}"
+
+# Given a string, return a new string made of 3 copies of the last 2 chars of the 
+# original string. The string length will be at least 2.
+def extra_end(str):
+    return 3 * str[-2:]
+
+# Given a string, return the string made of its first two chars, so the String "Hello" 
+# yields "He". If the string is shorter than length 2, return whatever there is, so "X" 
+# yields "X", and the empty string "" yields the empty string "". Note that len(str) returns 
+# the length of a string.
+def first_two(str):
+    return str[:2]
+
+# Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+def first_half(str):
+    return str[:(len(str) // 2)]
+
+# Given a string, return a version without the first and last char, so "Hello" yields "ell". The 
+# string length will be at least 2.
+def without_end(str):
+    return str[1:-1]
+
+# Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on 
+# the outside and the longer string on the inside. The strings will not be the same length, but they 
+# may be empty (length 0).
+def combo_string(a, b):
+    short, long = sorted([a, b], key=len)
+    return f"{short}{long}{short}"
+
+# Given 2 strings, return their concatenation, except omit the first char of each. The strings will 
+# be at least length 1.
+def non_start(a, b):
+    return f"{a[1:]}{b[1:]}"
--- tests/string1_tests.py
+++ tests/string1_tests.py
@@ -13,7 +13,7 @@ class TestString1(unittest.TestCase):
         self.assertEqual(string1.hello_name('Alpha'), 'Hello Alpha!')
         self.assertEqual(string1.hello_name('Omega'), 'Hello Omega!')
         self.assertEqual(string1.hello_name('Goodbye'), 'Hello Goodbye!')
-        self.assertEqual(string1.hello_name('ho ho ho'), 'Hello ho ho ho')
+        self.assertEqual(string1.hello_name('ho ho ho'), 'Hello ho ho ho!')
         self.assertEqual(string1.hello_name('xyz!'), 'Hello xyz!!')
         self.assertEqual(string1.hello_name('Hello'), 'Hello Hello!')
 
@@ -32,10 +32,77 @@ class TestString1(unittest.TestCase):
         self.assertEqual(string1.make_tags('i', 'Yay'), '<i>Yay</i>')
         self.assertEqual(string1.make_tags('i', 'Hello'), '<i>Hello</i>')
         self.assertEqual(string1.make_tags('cite', 'Yay'), '<cite>Yay</cite>')
-        self.assertEqual(string1.make_tags('address', 'here'), '<address>here</address')
+        self.assertEqual(string1.make_tags('address', 'here'), '<address>here</address>')
         self.assertEqual(string1.make_tags('body', 'Heart'), '<body>Heart</body>')
         self.assertEqual(string1.make_tags('i', 'i'), '<i>i</i>')
         self.assertEqual(string1.make_tags('i', ''), '<i></i>')
+
+    def test_make_out_word(self):
+        self.assertEqual(string1.make_out_word('<<>>>', 'Yay'), '<<Yay>>')
+        self.assertEqual(string1.make_out_word('<<>>', 'WooHoo'), '<<WooHoo>>')
+        self.assertEqual(string1.make_out_word('[[]]', 'word'), '[[word]]')
+        self.assertEqual(string1.make_out_word('HHoo', 'Hello'), 'HHHellooo')
+        self.assertEqual(string1.make_out_word('abyz', 'YAY'), 'abYAYyz')
+    
+    def test_extra_end(self):
+        self.assertEqual(string1.extra_end('Hello'), 'lololo')
+        self.assertEqual(string1.extra_end('ab'), 'ababab')
+        self.assertEqual(string1.extra_end('Hi'), 'HiHiHi')
+        self.assertEqual(string1.extra_end('Candy'), 'dydydy')
+        self.assertEqual(string1.extra_end('Code'), 'dedede')
+
+    def test_first_two(self):
+        self.assertEqual(string1.first_two('Hello'), 'He') 
+        self.assertEqual(string1.first_two('abcdefg'), 'ab') 
+        self.assertEqual(string1.first_two('ab'), 'ab') 
+        self.assertEqual(string1.first_two('a'), 'a') 
+        self.assertEqual(string1.first_two(''), '') 
+        self.assertEqual(string1.first_two('Kitten'), 'Ki') 
+        self.assertEqual(string1.first_two('hi'), 'hi') 
+        self.assertEqual(string1.first_two('hiya'), 'hi') 
+    
+    def test_first_half(self):
+        self.assertEqual(string1.first_half('WooHoo'), 'Woo')
+        self.assertEqual(string1.first_half('HelloThere'), 'Hello')
+        self.assertEqual(string1.first_half('abcdef'), 'abc')
+        self.assertEqual(string1.first_half('ab'), 'a')
+        self.assertEqual(string1.first_half(''), '')
+        self.assertEqual(string1.first_half('0123456789'), '01234')
+        self.assertEqual(string1.first_half('kitten'), 'kit')
+    
+    def test_without_end(self):
+        self.assertEqual(string1.without_end('Hello'), 'ell')
+        self.assertEqual(string1.without_end('java'), 'av')
+        self.assertEqual(string1.without_end('coding'), 'odin')
+        self.assertEqual(string1.without_end('code'), 'od')
+        self.assertEqual(string1.without_end('ab'), '')
+        self.assertEqual(string1.without_end('Chocolate!'), 'hocolate')
+        self.assertEqual(string1.without_end('kitten'), 'itte')
+        self.assertEqual(string1.without_end('woohoo'), 'ooho')
+    
+    def test_combo_string(self):
+        self.assertEqual(string1.combo_string('Hello','hi'), 'hiHellohi')
+        self.assertEqual(string1.combo_string('hi','Hello'), 'hiHellohi')
+        self.assertEqual(string1.combo_string('aaa','b'), 'baaab')
+        self.assertEqual(string1.combo_string('b','aaa'), 'baaab')
+        self.assertEqual(string1.combo_string('aaa',''), 'aaa')
+        self.assertEqual(string1.combo_string('','bb'), 'bb')
+        self.assertEqual(string1.combo_string('aaa','1234'), 'aaa1234aaa')
+        self.assertEqual(string1.combo_string('aaa','bb'), 'bbaaabb')
+        self.assertEqual(string1.combo_string('a','bb'), 'abba')
+        self.assertEqual(string1.combo_string('bb','a'), 'abba')
+        self.assertEqual(string1.combo_string('xyz','ab'), 'abxyzab')
+    
+    def test_non_start(self):
+        self.assertEqual(string1.non_start('Hello', 'There'), 'ellohere')
+        self.assertEqual(string1.non_start('java', 'code'), 'avaode')
+        self.assertEqual(string1.non_start('shotl', 'java'), 'hotlava')
+        self.assertEqual(string1.non_start('ab', 'xy'), 'by')
+        self.assertEqual(string1.non_start('ab', 'x'), 'b')
+        self.assertEqual(string1.non_start('x', 'ac'), 'c')
+        self.assertEqual(string1.non_start('a', 'x'), '')
+        self.assertEqual(string1.non_start('kit', 'kat'), 'itat')
+        self.assertEqual(string1.non_start('mart', 'dart'), 'artart')
 
 if __name__ == '__main__':
     unittest.main()
