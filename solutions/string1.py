39e4833d63b6dd87b130c8c355abb668842b0730
cfevrius <21692588+cfevrius@users.noreply.github.com>
2022-11-13 16:02:59 -0500

    Added solutions for problems 18-21 and started adding test code for problems 22-33.

--- solutions/string1.py
+++ solutions/string1.py
@@ -1,3 +1,5 @@
+import re 
+
 # Given a string name, e.g. "Bob", return a 
 # greeting of the form "Hello Bob!".
 def hello_name(name):
@@ -74,13 +76,13 @@ def the_end(str, front):
 
 # Given a string, return a version without both the first and last char of the string. The string may be any length, including 0.
 def without_end_2(str):
-    return f"{str[1:-1]}"
+    return str[1:-1]
 
 # Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length 
 # will be at least 2.
 def middle_two(str):
     start = len(str) // 2 - 1
-    return f"{str[start:start + 2]}"
+    return str[start:start + 2]
 
 # Given a string, return true if it ends in "ly".
 def ends_ly(str):
@@ -89,3 +91,80 @@ def ends_ly(str):
 # Given a string and an int n, return a string made of the first and last n chars from the string. The string length will be at least n.
 def n_twice(str, n):
     return "" if n == 0 else f"{str[:n]}{str[-n:]}"
+
+# Given a string and an index, return a string length 2 starting at the given index. If the index is too big or too small to define a string 
+# length 2, use the first 2 chars. The string length will be at least 2.
+def two_char(str, index):
+    return str[:2] if (index > (len(str) - 2) or index < 0) else str[index:index+2]
+
+# Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
+def middle_three(str):
+    index = len(str) // 2 - 1
+    return str[index: index + 3]
+
+# Given a string, return true if "bad" appears starting at index 0 or 1 in the string, such as with "badxxx" or "xbadxx" but not "xxbadxx". The 
+# string may be any length, including 0. Note: use == to compare 2 strings.
+def has_bad(str):
+    matches = [i for i, _ in enumerate(str[-3:]) if str[i:i+3] == 'bad']
+    return 0 in matches or 1 in matches
+
+# Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2, use '@' for the missing chars.
+def at_first(str):
+    return f"{str[:2]}{'@' * (2 - len(str))}"
+
+# Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo" and "java" yields "ya". If either 
+# string is length 0, use '@' for its missing char.
+def last_chars(a, b):
+    return None
+
+# Given two strings, append them together (known as "concatenation") and return the result. However, if the concatenation creates a double-char, then 
+# omit one of the chars, so "abc" and "cat" yields "abcat".
+def con_cat(a, b):
+    return None
+
+# Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign".
+def last_two(str):
+    return None
+
+# Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty string.
+def see_color(str):
+    return None
+
+# Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as with "edited".
+def front_again(str):
+    return None
+
+# Given two strings, append them together (known as "concatenation") and return the result. However, if the strings are different lengths, 
+# omit chars from the longer string so it is the same length as the shorter string. So "Hello" and "Hi" yield "loHi". The strings may be any length.
+def min_cat(a, b):
+    return None
+
+# Given a string, return a new string made of 3 copies of the first 2 chars of the original string. The string may be any length. If there are fewer than 
+# 2 chars, use whatever is there.
+def extra_front(str):
+    return None
+
+# Given a string, if a length 2 substring appears at both its beginning and end, return a string without the substring at the beginning, so "HelloHe" yields 
+# "lloHe". The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
+def without2(str):
+    return None
+
+# Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the second char if it is 'b'. The string may be any 
+# length. Harder than it looks.
+def de_front(str):
+    return None
+
+# Given a string and a second "word" string, we'll say that the word matches the string if it appears at the front of the string, except its first char does not need 
+# to match exactly. On a match, return the front of the string, or otherwise return the empty string. So, so with the string "hippo" the word "hi" returns "hi" and "xip" 
+# returns "hip". The word will be at least length 1.
+def start_word(str, word):
+    return None
+
+# Given a string, if the first or last chars are 'x', return the string without those 'x' chars, and otherwise return the string unchanged.
+def without_x(str):
+    return None
+
+# Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars, and otherwise return the string unchanged. 
+# This is a little harder than it looks.
+def without_x_2(str):
+    return None
