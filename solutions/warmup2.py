ed4fb05ded18188c70fd47302b83e073d1fc95f9
cfevrius <21692588+cfevrius@users.noreply.github.com>
2022-11-26 23:06:43 -0500

    Removed all references of regex solutions.

--- solutions/warmup2.py
+++ solutions/warmup2.py
@@ -1,5 +1,3 @@
-import re
-
 # Given a string and a non-negative int n, return 
 # a larger string that is n copies of the original string.
 def string_times(str, n):
@@ -15,16 +13,12 @@ def front_times(str, n):
 
 # Count the number of "xx" in the given string. We'll say that 
 # overlapping is allowed, so "xxx" contains 2 "xx".
-#
-# (NOTE) Look into regex solution.
 def count_xx(str):
     matches = [i for i, _ in enumerate(str[:-1]) if str[i:i+2] == 'xx']
     return len(matches)
 
 # Given a string, return true if the first instance of "x" in 
 # the string is immediately followed by another "x".
-# 
-# (NOTE) Look into regex solution.
 def double_x(str):
     first_occur = str.find('x')
     second_occur = str.find('x', first_occur + 1)
@@ -43,8 +37,6 @@ def string_splosion(str):
 # Given a string, return the count of the number of times that a substring 
 # length 2 appears in the string and also as the last 2 chars of the string, 
 # so "hixxxhi" yields 1 (we won't count the end substring).
-# 
-# (NOTE) Look into regex solution.
 def last2(str):
     search_str = str[-2:]
     occurrences = [i for i in range(len(str)) if str.startswith(search_str, i, -1)]
@@ -53,8 +45,6 @@ def last2(str):
 # Given an array of ints, return the number of 9's in the array.
 def array_count9(nums):
     return len([i for i in nums if i == 9])
-    # str_nums = ''.join([str(x) for x in nums])
-    # return len(re.findall('9', str_nums))
 
 # Given an array of ints, return true if one of the first 4 elements in the array 
 # is a 9. The array length may be less than 4.
@@ -64,8 +54,6 @@ def array_front9(nums):
 
 # Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears 
 # in the array somewhere.
-# 
-# (NOTE) Look into regex solution.
 def array123(nums):
     occurrences = [i for i, _ in enumerate(nums[:-2]) if nums[i:i+3] == [1, 2, 3]]
     return len(occurrences) > 0
@@ -80,7 +68,6 @@ def string_match(a, b):
 
 # Given a string, return a version where all the "x" have been removed. 
 # Except an "x" at the very start or end should not be removed.
-# (NOTE): Look into regex solution.
 def string_x(str):
     middle = ''.join([char for char in str[1:-1] if char != 'x'])
     return f"{str[0]}{middle}{str[-1]}" if len(str) > 1 else str
@@ -92,10 +79,10 @@ def alt_pairs(str):
 
 # Given a string, return a version where all the 'yak' are removed, but the 'a'
 # can by any char. The 'yak' string will not overlap.
-#
-# (NOTE): Find non-regex soution
 def string_yak(str):
-    return ''.join(re.split(r'y.k', str))
+    indexes = [(i, i+1, i+2) for i, _ in enumerate(str[:-2]) if str[i] == 'y' and str[i+2] == 'k' ]
+    flatten_indexes = [element for item in indexes for element in item]
+    return ''.join([x for i, x in enumerate(str) if i not in flatten_indexes])
 
 # Given an array of ints, return the number of times that two 6's are next to each other
 # in the array. Also count instances where the second '6' is actually a 7.
@@ -103,19 +90,11 @@ def array_667(nums):
     matches = [i for i, x in enumerate(nums[:-1]) if nums[i] == 6 and nums[i+1] in {6, 7}] 
     return len(matches)
 
-    # Regex solution:
-    # str_nums = ''.join(str(x) for x in nums)
-    # return len(re.findall(r'(?=(6[67]))', str_nums))
-
 # Given an array of ints, we'll say that a triple is a value appearing 3 times in a
 # row in the array. Return true if the array does not contain any triples.
 def no_triples(nums):
     matches = [v for i, v in enumerate(nums[:-2]) if nums[i] == nums[i+1] == nums[i+2]]
     return len(matches) == 0
-
-    # Regex solution:
-    # str_nums = ''.join(str(x) for x in nums)
-    # return len(re.findall(r'(\d)\1\1', str_nums)) == 0  
 
 # Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value, followed 
 # by the value plus 5, followed by the value minus 1. Additionally the 271 counts even if 
