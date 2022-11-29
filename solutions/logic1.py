c5c59d470f5f7d86612e28da4be7852ea0687f00
cfevrius <21692588+cfevrius@users.noreply.github.com>
2022-11-29 01:24:31 -0500

    More Logic-1 solutions.

--- solutions/logic1.py
+++ solutions/logic1.py
@@ -190,13 +190,26 @@ def max_mod_5(a, b):
 # all the value 2, the result is 10. Otherwise if they are all the same, the result is 5. Otherwise 
 # so long as both b and c are different from a, the result is 1. Otherwise the result is 0.
 def red_ticket(a, b, c):
-    return None
+    if all([a == 2, b == 2, c == 2]):
+        return 10
+    elif a == b == c:
+        return 5
+    elif b != a != c:
+        return 1
+    else:
+        return 0
 
 # You have a green lottery ticket, with ints a, b, and c on it. If the numbers are all different 
 # from each other, the result is 0. If all of the numbers are the same, the result is 20. If two 
 # of the numbers are the same, the result is 10.
 def green_ticket(a, b, c):
-    return None
+    set_size = len(set([a, b, c]))
+    if set_size == 3:
+        return 0
+    elif set_size == 1:
+        return 20
+    elif set_size == 2:
+        return 10
 
 # You have a blue lottery ticket, with ints a, b, and c on it. This makes three pairs, which we'll 
 # call ab, bc, and ac. Consider the sum of the numbers in each pair. If any pair sums to exactly 10,
