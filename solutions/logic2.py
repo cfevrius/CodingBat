21c988106efc1c69589a8eda2c63b79e09310ec8
cfevrius <21692588+cfevrius@users.noreply.github.com>
2022-12-01 19:03:34 -0500

    More solutions

--- solutions/logic2.py
+++ solutions/logic2.py
@@ -18,44 +18,60 @@ def lone_sum(a, b, c):
 # then it does not count towards the sum and values to its right do not count. So 
 # for example, if b is 13, then both b and c do not count.
 def lucky_sum(a, b, c):
-    return None 
+    nums = (a, b, c)
+    index_13 = [index for index, val in enumerate(nums) if val == 13]
+    new_lst = nums if not index_13 else nums[:index_13[0]]
+    return sum(new_lst)
 
 # Given 3 int values, a b c, return their sum. However, if any of the values is a teen 
 # -- in the range 13..19 inclusive -- then that value counts as 0, except 15 and 16 do 
-# not count as a teens. Write a separate helper "public int fixTeen(int n) {"that takes 
+# not count as a teens. Write a separate lambda function, fix_teen, that takes 
 # in an int value and returns that value fixed for the teen rule. In this way, you avoid 
-# repeating the teen code 3 times (i.e. "decomposition"). Define the helper below and at 
-# the same indent level as the main noTeenSum().
+# repeating the teen code 3 times (i.e. "decomposition"). Define the lambda function 
+# within the body of no_teen_sum. 
 def no_teen_sum(a, b, c):
-    return None
+    fix_teen = lambda num : 0 if 13 <= num <= 19 and num not in {15, 16} else num
+    fixed_nums = map(fix_teen, (a, b, c))
+    return sum(fixed_nums)
 
 # For this problem, we'll round an int value up to the next multiple of 10 if its rightmost
 # digit is 5 or more, so 15 rounds up to 20. Alternately, round down to the previous multiple 
 # of 10 if its rightmost digit is less than 5, so 12 rounds down to 10. Given 3 ints, a b c, 
-# return the sum of their rounded values. To avoid code repetition, write a separate helper 
-# "public int round10(int num) {" and call it 3 times. Write the helper entirely below and 
-# at the same indent level as roundSum().
+# return the sum of their rounded values. To avoid code repetition, write a separate lambda 
+# function, round_10, and call it 3 times. Write the helper entirely below and 
+# at the same indent level as round_sum().
 def round_sum(a, b, c):
-    return None
+    round_up = lambda n : n + (10 - (n % 10))
+    round_down = lambda n : n - (n % 10)
+    round_10 = lambda n : round_down(n) if (n % 10) < 5 else round_up(n)
+    return sum(map(round_10, (a, b, c)))
 
 # Given three ints, a b c, return true if one of b or c is "close" (differing from a by at 
 # most 1), while the other is "far", differing from both other values by 2 or more. Note: 
 # abs(num) computes the absolute value of a number.
 def close_far(a, b, c):
-    return None
+    close = lambda num, target: abs(num - target) <= 1
+    far = lambda num, target1, target2: abs(num - target1) >= 2 and abs(num - target2) >= 2
+    return (close(b, a) and far(c, a, b)) or (close(c, a) and far(b, a, c))
 
 # Given 2 int values greater than 0, return whichever value is nearest to 21 without going over. 
 # Return 0 if they both go over. 
 def blackjack(a, b):
-    return None
+    diff_by_21 = lambda n : abs(21 - n)
+    if a > 21 and b > 21:
+        return 0
+    else:
+        nums_that_are_lower = (n for n in (a, b) if n <= 21)
+        return min(nums_that_are_lower, key=diff_by_21)
 
 # Given three ints, a b c, one of them is small, one is medium and one is large. Return true if the
 # three values are evenly spaced, so the difference between small and medium is the same as the 
 # difference between medium and large.
 def evenly_spaces(a, b, c):
-    return None
+    small, medium, large = sorted((a, b, c))
+    return medium - small == large - medium
 
-# We want make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars 
+# We want to make a package of goal kilos of chocolate. We have small bars (1 kilo each) and big bars 
 # (5 kilos each). Return the number of small bars to use, assuming we always use big bars before 
 # small bars. Return -1 if it can't be done.
 def make_chocolate(small, big, goal):
