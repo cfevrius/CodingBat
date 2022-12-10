b358656272a1fc8863ecdbeaf75e628c05f91f40
cfevrius <21692588+cfevrius@users.noreply.github.com>
2022-12-09 23:59:35 -0500

    Started work on Array 2 tests.

---
+++ tests/array2_tests.py
@@ -0,0 +1,27 @@
+# Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+def count_evens(nums):
+    return None
+
+# Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. Note: the built-in Math.min(v1, v2) and Math.max(v1, v2) methods return the smaller or larger of two values.
+def big_diff(nums):
+    return None
+
+# Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.
+def centered_average(nums):
+    return None
+
+# Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+def sum_13(nums):
+    return None
+
+# Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+def sum_67(nums):
+    return None
+
+# Given an array of ints, return true if the array contains a 2 next to a 2 somewhere.
+def has_22(nums):
+    return None
+
+# Given an array of ints, return true if the array contains no 1's and no 3's.
+def lucky_13(nums):
+    return None
