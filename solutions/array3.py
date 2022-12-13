e785598b4fc483f79d00e4e724ae6f7060674c3f
cfevrius <21692588+cfevrius@users.noreply.github.com>
2022-12-13 11:02:22 -0500

    Changed naming convention for test files. Also added a few solutions to Array 3 problems.

--- solutions/array3.py
+++ solutions/array3.py
@@ -2,14 +2,32 @@
 # "span" is the number of elements between the two inclusive. A single value has a span of 1. 
 # Returns the largest span found in the given array. (Efficiency is not a priority.)
 def max_span(nums):
-    return None
+    nums_str = ''.join(str(num) for num in nums)
+    def find_span(num):
+        first = nums_str.index(str(num))
+        last = nums_str.rindex(str(num))
+        return (last - first) + 1
+    all_spans = [find_span(v) for i, v in enumerate(nums)]
+    return max(all_spans) if all_spans else 0
 
 # Return an array that contains exactly the same numbers as the given array, but rearranged so 
 # that every 3 is immediately followed by a 4. Do not move the 3's, but every other number may 
 # move. The array contains the same number of 3's and 4's, every 3 has a number after it that is 
 # not a 3, and a 3 appears in the array before any 4.
 def fix_34(nums):
-    return None
+    index_of_4s = [i for i, val in enumerate(nums) if val == 4]
+
+    def swap(index1, index2):
+        nums[index1], nums[index2] = nums[index2], nums[index1]
+
+    for index in index_of_4s:
+        if nums[index - 1] != 3:
+            available_3s = [i 
+                            for i, val in enumerate(nums) 
+                            if val == 3 and nums[i+1] != 4]
+            if available_3s:
+                swap(index, available_3s[0] + 1)
+    return nums
 
 # (This is a slightly harder version of the fix34 problem.) Return an array that contains exactly 
 # the same numbers as the given array, but rearranged so that every 4 is immediately followed by 
@@ -17,7 +35,19 @@ def fix_34(nums):
 # of 4's and 5's, and every 4 has a number after it that is not a 4. In this version, 5's may 
 # appear anywhere in the original array.
 def fix_45(nums):
-    return None
+    index_of_5s = [i for i, val in enumerate(nums) if val == 5]
+
+    def swap(index1, index2):
+        nums[index1], nums[index2] = nums[index2], nums[index1]
+    
+    for index in index_of_5s:
+        if nums[index - 1] != 4:
+            available_4s = [i 
+                            for i, val in enumerate(nums) 
+                            if val == 4 and nums[i + 1] != 5]
+            if available_4s:
+                swap(index, available_4s[0] + 1)
+    return nums
 
 # Given a non-empty array, return true if there is a place to split the array so that the sum of 
 # the numbers on one side is equal to the sum of the numbers on the other side.
