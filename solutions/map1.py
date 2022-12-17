c11d9db08704c7226c0d0d99ccca8d5581f8b978
cfevrius <21692588+cfevrius@users.noreply.github.com>
2022-12-16 20:01:55 -0500

    Solved all Map-1 problems.

--- solutions/map1.py
+++ solutions/map1.py
@@ -1,51 +1,82 @@
-# Modify and return the given map as follows: if the key "a" has a value, set the key "b" 
-# to have that value, and set the key "a" to have the value "". Basically "b" is a bully, 
+# Modify and return the given map as follows: if the key 'a' has a value, set the key 'b' 
+# to have that value, and set the key 'a' to have the value ''. Basically 'b' is a bully, 
 # taking the value and replacing it with the empty string.
-def map_bully(map):
-    return None
+def map_bully(mp):
+    if 'a' in mp:
+        mp['a'], mp['b'] = '', mp['a']
+    return mp
 
-# Modify and return the given map as follows: if the key "a" has a value, set the key "b" 
-# to have that same value. In all cases remove the key "c", leaving the rest of the map 
+# Modify and return the given map as follows: if the key 'a' has a value, set the key 'b' 
+# to have that same value. In all cases remove the key 'c', leaving the rest of the map 
 # unchanged.
-def map_share(map):
-    return None
+def map_share(mp):
+    if 'a' in mp:
+        mp['b'] = mp['a']
+    if 'c' in mp:
+        del mp['c']
+    return mp
 
 # Modify and return the given map as follows: for this problem the map may or may not contain 
-# the "a" and "b" keys. If both keys are present, append their 2 string values together and 
-# store the result under the key "ab".
-def map_ab(map):
-    return None
+# the 'a' and 'b' keys. If both keys are present, append their 2 string values together and 
+# store the result under the key 'ab'.
+def map_ab(mp):
+    if 'a' in mp and 'b' in mp:
+        mp['ab'] = mp['a'] + mp['b']
+    return mp
 
 # Given a map of food keys and topping values, modify and return the map as follows: if the 
-# key "ice cream" is present, set its value to "cherry". In all cases, set the key "bread" 
-# to have the value "butter".
-def topping1(map):
-    return None
+# key 'ice cream' is present, set its value to 'cherry'. In all cases, set the key 'bread' 
+# to have the value 'butter'.
+def topping1(mp):
+    if 'ice cream' in mp:
+        mp['ice cream'] = 'cherry'
+    mp['bread'] = 'butter'
+    return mp
 
 # Given a map of food keys and their topping values, modify and return the map as follows: 
-# if the key "ice cream" has a value, set that as the value for the key "yogurt" also. If 
-# the key "spinach" has a value, change that value to "nuts".
-def topping2(map):
-    return None
+# if the key 'ice cream' has a value, set that as the value for the key 'yogurt' also. If 
+# the key 'spinach' has a value, change that value to 'nuts'.
+def topping2(mp):
+    if 'ice cream' in mp:
+        mp['yogurt'] = mp['ice cream']
+    if 'spinach' in mp:
+        mp['spinach'] = 'nuts'
+    return mp
 
 # Given a map of food keys and topping values, modify and return the map as follows: if 
-# the key "potato" has a value, set that as the value for the key "fries". If the key "salad" 
-# has a value, set that as the value for the key "spinach".
-def topping3(map):
-    return None
+# the key 'potato' has a value, set that as the value for the key 'fries'. If the key 'salad' 
+# has a value, set that as the value for the key 'spinach'.
+def topping3(mp):
+    if 'potato' in mp:
+        mp['fries'] = mp['potato']
+    if 'salad' in mp:
+        mp['spinach'] = mp['salad']
+    return mp
 
-# Modify and return the given map as follows: if the keys "a" and "b" are both in the map 
+# Modify and return the given map as follows: if the keys 'a' and 'b' are both in the map 
 # and have equal values, remove them both.
-def map_ab2(map):
-    return None
+def map_ab2(mp):
+    if 'a' in mp and 'b' in mp and mp['a'] == mp['b']:
+        del mp['a']
+        del mp['b']
+    return mp
 
-# Modify and return the given map as follows: if exactly one of the keys "a" or "b" has a 
+# Modify and return the given map as follows: if exactly one of the keys 'a' or 'b' has a 
 # value in the map (but not both), set the other to have that same value in the map.
-def map_ab3(map):
-    return None
+def map_ab3(mp):
+    if 'a' in mp and 'b' not in mp:
+        mp['b'] = mp['a']
+    elif 'b' in mp and 'a' not in mp:
+        mp['a'] = mp['b']
+    return mp
 
-# Modify and return the given map as follows: if the keys "a" and "b" have values that have 
-# different lengths, then set "c" to have the longer value. If the values exist and have the 
+# Modify and return the given map as follows: if the keys 'a' and 'b' have values that have 
+# different lengths, then set 'c' to have the longer value. If the values exist and have the 
 # same length, change them both to the empty string in the map.
-def map_ab4(map):
-    return None
+def map_ab4(mp):
+    if 'a' in mp and 'b' in mp:
+        if len(mp['a']) == len(mp['b']):
+            mp['a'], mp['b'] = '', ''
+        else:
+            mp['c'] = max(mp['a'], mp['b'], key=len)
+    return mp
